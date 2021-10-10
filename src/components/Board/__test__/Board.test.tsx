46156e526a12e8e368d820747e7d324ea0954a63
Wasswa Richard <wasswa@awamo.com>
2021-10-10 11:26:26 +0300

    refactor project

--- src/components/Board/__test__/Board.test.tsx
+++ src/components/Board/__test__/Board.test.tsx
@@ -4,6 +4,9 @@ import Enzyme, { shallow } from 'enzyme';
 import { cleanup } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-16';
 import TestRenderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import BoardColumn from '../../BoardColumn/BoardColumn';
+import configureStore from 'redux-mock-store';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Board>', () => {
@@ -13,13 +16,19 @@ describe('<Board>', () => {
     { userId: 2, id: 2, title: 'DONE', completed: true, status: 'DONE' },
   ];
   const statusChange = () => {};
+  const mockStore = configureStore();
+  let store;
+
   it('renders BoardColumn Component', () => {
     shallow(<Board todos={todoElements} setStatusChange={statusChange} />);
   });
 
   it('should match snapshot', function () {
+    store = mockStore(todoElements);
     const tree = TestRenderer.create(
-      <Board todos={todoElements} setStatusChange={statusChange} />
+      <Provider store={store}>
+        <Board todos={todoElements} setStatusChange={statusChange} />
+      </Provider>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
--- src/components/BoardColumn/__test__/BoardColumn.test.tsx
+++ src/components/BoardColumn/__test__/BoardColumn.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import BoardColumn from '../BoardColumn';
 import Enzyme, { shallow } from 'enzyme';
-import { cleanup } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-16';
 import TestRenderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import TodosList from '../../TodosList/TodosList';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<BoardColumn>', () => {
@@ -13,23 +16,31 @@ describe('<BoardColumn>', () => {
     { userId: 2, id: 2, title: 'DONE', completed: true, status: 'DONE' },
   ];
   const statusChange = () => {};
+  const mockStore = configureStore();
+  let store;
   it('renders BoardColumn Component', () => {
-    shallow(
-      <BoardColumn
-        title="DONE"
-        todos={todoElements}
-        setStatusChange={statusChange}
-      />
+    store = mockStore(todoElements);
+    render(
+      <Provider store={store}>
+        <BoardColumn
+          title="DONE"
+          todos={todoElements}
+          setStatusChange={statusChange}
+        />
+      </Provider>
     );
   });
 
   it('should match snapshot', function () {
+    store = mockStore(todoElements);
     const tree = TestRenderer.create(
-      <BoardColumn
-        title="DONE"
-        todos={todoElements}
-        setStatusChange={statusChange}
-      />
+      <Provider store={store}>
+        <BoardColumn
+          title="DONE"
+          todos={todoElements}
+          setStatusChange={statusChange}
+        />
+      </Provider>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
